ec85b64d498b99dbef7a2880c624e169c0ee500a
Rodolfo Fernandez <rodyy@outlook.com>
2017-04-13 00:00:48 -0300

    proyecto de prueba con firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  contactos: FirebaseListObservable<any[]>
+ constructor(af: AngularFire){
+   this.contactos = af.database.list('/contactos');
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDWuwf5DaMytiIbUKhKlniXHwDdY9r5j90",
+    authDomain: "contactos-52c43.firebaseapp.com",
+    databaseURL: "https://contactos-52c43.firebaseio.com",
+    projectId: "contactos-52c43",
+    storageBucket: "contactos-52c43.appspot.com",
+    messagingSenderId: "335462454032"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
